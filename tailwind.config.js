35721c80a2acb01aa9012a9e3de70bbd90fe89fd
koladeFaith <koladefaith1000@gmail.com>
2024-07-19 13:19:44 -0700

    Hero's section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}", "./index.html"],
   theme: {
-    extend: { fontFamily: { rubik: '"Rubik",san-serif' } },
+    extend: {
+      fontFamily: { rubik: '"Rubik",san-serif' },
+      colors: {
+        main: "#AC702F",
+      },
+    },
   },
   plugins: [],
 };
